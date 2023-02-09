040309fd38f0992d633499f23846473d16ef5dab
dumbsito <lean.04river@gmail.com>
2023-02-09 17:21:37 -0300

    dou

--- index.js
+++ index.js
@@ -29,7 +29,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 require("./config/passport")(passport);
 
 app.use("/users", users);
